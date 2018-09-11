b8ab48f4641ab44dc55fa3630a332f6a7798bb9e
bratyn20 <bratyn20@gmail.com>
2018-09-11 11:48:58 +0700

    Загрузка файлов в репозиторий

---
+++ partyInvites/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using partyInvites.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace partyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            //return "Hello world";
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Доброе утро братишка" : "Добрый вечер брачо";
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guest)
+        {
+            if (ModelState.IsValid)
+                // Нужно отправить данные нового гостя по электронной почте 
+                // организатору вечеринки.
+                return View("Thanks", guest);
+            else
+                // Обнаружена ошибка проверки достоверности
+                return View();
+        }
+    }
+}
